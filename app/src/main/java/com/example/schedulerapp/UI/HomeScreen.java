65abef133f2c4fec1828c3396e296ab72980ba9d
CodeArmon <codeworld62@gmail.com>
2022-12-11 22:10:14 -0500

    menu add sample data

--- app/src/main/java/com/example/schedulerapp/UI/HomeScreen.java
+++ app/src/main/java/com/example/schedulerapp/UI/HomeScreen.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.schedulerapp.Database.Repository;
 import com.example.schedulerapp.R;
+import com.example.schedulerapp.entities.Assessment;
+import com.example.schedulerapp.entities.Course;
 import com.example.schedulerapp.entities.Term;
 
 
@@ -19,9 +23,9 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Button button=findViewById(R.id.button);
-        Term term= new Term(0,"Spring", "12-14-2002","12-15-2022");
+       /* Term term= new Term(0,"Spring", "12-14-2002","12-15-2022");
         Repository repository = new Repository(getApplication());
-        repository.insert(term);
+        repository.insert(term); */
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,5 +33,24 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.addSampleData:
+                Term term= new Term(0,"Spring", "12-14-2002","12-15-2022");
+                Repository repository = new Repository(getApplication());
+                repository.insert(term);
+                Assessment assessment= new Assessment(0,"Performance","12-20-2022",2);
+                repository.insert(assessment);
+                Course course=new Course(1, 2, "Science","12-17-2022","02-10-2023","Professor Xavier","404-555-0912","profx@school.edu");
+                repository.insert(course);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
